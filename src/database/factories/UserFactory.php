6a4aa14b99290233e0a73dd4f5b2a3a69b65c6bd
saaaaku <saaaaku@gmail.com>
2026-05-07 22:16:26 +0900

    fix: UserFactory for Test

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -20,6 +20,10 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+
+            'postal_code' => '123-4567',
+            'address' => '東京都新宿区1-1-1',
+            'building' => 'テストマンション101',
         ];
     }
 
